7779a8ac89a589952fc47a5dc55559b22563c98b
boginw <bogi.wennerstrom@gmail.com>
2017-09-16 17:32:17 +0200

    Tests and appveyor

---
+++ AAU Cal/CalScraper/CalScraper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAU_Cal.CalScraper {
+	public class CalScraper {
+		public CalScraper() {
+
+		}
+
+	}
+}
---
+++ AAU CalTests/CalScraper/CalScraperTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using AAU_Cal.CalScraper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAU_Cal.CalScraper.Tests {
+	[TestFixture()]
+	public class CalScraperTests {
+		[Test()]
+		public void CalScraperTest() {
+
+		}
+	}
+}
