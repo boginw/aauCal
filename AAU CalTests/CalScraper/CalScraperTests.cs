5aacd3e39223b6cf59d4a2a05e5bfa1e77954dbd
Beinir <beinir.ragnuson@gmail.com>
2017-09-16 23:28:31 +0200

    Created webscraper and parsed lectures

--- AAU CalTests/CalScraper/CalScraperTests.cs
+++ AAU CalTests/CalScraper/CalScraperTests.cs
@@ -5,13 +5,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AAU_Cal.Objects;
 
 namespace AAU_Cal.CalScraper.Tests {
-	[TestFixture()]
-	public class CalScraperTests {
-		[Test()]
-		public void CalScraperTest() {
+    [TestFixture()]
+    public class CalScraperTests {
+        [Test()]
+        public void CanInitializeSraperClass()
+        {
+            CalScraper scraper = new CalScraper("https://www.moodle.aau.dk/calmoodle/public/?sid=4334");
+        }
 
-		}
-	}
+        [Test()]
+        public void CanDownloadPage(){
+            CalScraper scraper = new CalScraper("https://www.moodle.aau.dk/calmoodle/public/?sid=4334");
+            string page = scraper.GetPage();
+            string expected = "Schedule for Computer Science / Software 3, E17";
+
+            Assert.True(page.Contains(expected));
+        }
+
+        [Test()]
+        public void CanParseHtml()
+        {
+            CalScraper scraper = new CalScraper("https://www.moodle.aau.dk/calmoodle/public/?sid=4334");
+            LectureCont[] lectures = scraper.GetLectures();
+        }
+    }
+
+
 }
